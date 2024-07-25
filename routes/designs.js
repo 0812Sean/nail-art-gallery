5bc1ef9cf4761c2f87c7e05e95bf16888b0d3d68
0812Sean <dachenxuan@gmail.com>
2024-07-25 17:08:53 -0500

    A

--- routes/designs.js
+++ routes/designs.js
@@ -10,58 +10,82 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 // Create Design
 router.post('/', isLoggedIn, async (req, res) => {
-  const newDesign = new Design(req.body.design);
-  newDesign.author = req.session.userId;
-  if (req.file) {
-    newDesign.image = `/uploads/${req.file.filename}`;
+  try {
+    const newDesign = new Design({
+      ...req.body.design,
+      author: req.session.userId,
+      image: req.file ? `/uploads/${req.file.filename}` : undefined
+    });
+    await newDesign.save();
+    res.redirect(`/designs/${newDesign._id}`);
+  } catch (error) {
+    console.error(error);
+    res.redirect('/new');
   }
-  await newDesign.save();
-  res.redirect(`/designs/${newDesign._id}`);
 });
 
 // Show Design
 router.get('/:id', async (req, res) => {
-    try {
-      const design = await Design.findById(req.params.id)
-        .populate('author')
-        .populate({
-          path: 'comments',
-          populate: { path: 'author' }
-        });
-      res.render('designs/show', { design });
-    } catch (e) {
-      console.error(e);
-      res.redirect('/');
-    }
-  });
+  try {
+    const design = await Design.findById(req.params.id)
+      .populate('author')
+      .populate({
+        path: 'comments',
+        populate: { path: 'author' }
+      });
+    res.render('designs/show', { design });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
 
 // Edit Design
 router.get('/:id/edit', isLoggedIn, async (req, res) => {
-  const design = await Design.findById(req.params.id);
-  res.render('designs/edit', { design });
+  try {
+    const design = await Design.findById(req.params.id);
+    res.render('designs/edit', { design });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
 });
 
 // Update Design
 router.put('/:id', isLoggedIn, async (req, res) => {
-  const design = await Design.findByIdAndUpdate(req.params.id, req.body.design);
-  if (req.file) {
-    design.image = `/uploads/${req.file.filename}`;
+  try {
+    const design = await Design.findByIdAndUpdate(req.params.id, req.body.design, { new: true });
+    if (req.file) {
+      design.image = `/uploads/${req.file.filename}`;
+    }
+    await design.save();
+    res.redirect(`/designs/${design._id}`);
+  } catch (error) {
+    console.error(error);
+    res.redirect(`/designs/${req.params.id}/edit`);
   }
-  await design.save();
-  res.redirect(`/designs/${design._id}`);
 });
 
 // Delete Design
 router.delete('/:id', isLoggedIn, async (req, res) => {
-  await Design.findByIdAndDelete(req.params.id);
-  res.redirect('/home');
+  try {
+    await Design.findByIdAndDelete(req.params.id);
+    res.redirect('/home');
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
 });
 
 // Filter by Category
 router.get('/category/:category', async (req, res) => {
-    const category = req.params.category;
-    const designs = await Design.find({ category }).populate('author');
+  try {
+    const designs = await Design.find({ category: req.params.category }).populate('author');
     res.render('index', { designs });
-  });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
 
 module.exports = router;
